6e951e8c24aeb0bd77c19a2383b55c8e46936105
Sasha Trubetskoy <sasha@Sashas-iMac.local>
2018-02-19 23:24:27 -0600

    sorta fixed capcha issue

--- michigan/facebook/get_mi_school_pages.py
+++ michigan/facebook/get_mi_school_pages.py
@@ -1,65 +1,38 @@
 import re
+import random
 from time import sleep
-from random import random
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
 
 
-URL_STOP_WORDS = ['pages', 'profile.', 'Elementary']
+URL_STOP_WORDS = ['pages', 'profile.', 'HS', 'highschool', 'Elementary', 'Middle School', 'Middle-School', 'High School']
+ENGINES = ['google']
+USER_AGENTS = ['Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36']
 
 
-def find_best(res_list):
-    result_likes = []
-    for l in res_list:
-        try:
-            url = l.find_element(By.XPATH, './/h3[@class="r"]/a[@href]').get_attribute('href')
-            text = l.find_element(By.XPATH, './/div[@class="s"]/div/span[@class="st"]').text
-        except StaleElementReferenceException:
-            break
+def start_browser():
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("general.useragent.override", random.choice(USER_AGENTS))
+    fp.update_preferences()
 
-        if any(w in url for w in URL_STOP_WORDS) or ('facebook' not in url) or ('likes' not in text):
-            # print('url', url)
-            # print('text', text, '\n')
-            continue
-        else:
-            num_likes = re.findall(r'(\d+)\slikes', l.text)
-            k_likes = re.findall(r'([\d\.]+)K\slikes', l.text)
-            if num_likes:
-                result_likes.append((url, int(num_likes[0])))
-            elif k_likes:
-                result_likes.append((url, int(float(k_likes[0]) * 1000)))
+    browser = webdriver.Firefox(firefox_profile=fp)
+    browser.implicitly_wait(10)
+    return fp, browser
+    
 
-    sor = sorted(result_likes, key=lambda tup: tup[1], reverse=True)
-    print('sorted candidates:\n', sor)
-    if not sor:
-        return 'NOT FOUND'
-
-    return sor[0][0]
-
-
-def get_fb_name(query):
-    input_element = driver.find_element_by_name("q")
-    sleep(.75 + random()-0.5)
-    input_element.send_keys(query)
-    sleep(.75 + random()-0.5)
-    input_element.submit()
-    Y = random()*1080
-    driver.execute_script("window.scrollTo(0, {});".format(Y))
-
-    RESULTS_LOCATOR = "//div[@class='rc']"
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, RESULTS_LOCATOR)))
-    rcs = driver.find_elements(By.XPATH, RESULTS_LOCATOR)
-    best_result = find_best(rcs)
-
-    if len(best_result.split('/')) >= 3:
-        fb_name = best_result.split('/')[3].split('?')[0]
-        return fb_name
-    else:
-        return 'NOT FOUND'
+def get_district_names(filename):
+    with open(filename) as f:
+        dists = f.readlines()
+    return dists
 
 
 def get_query(dist_name):
@@ -72,28 +45,147 @@ def get_query(dist_name):
     return query
 
 
-driver = webdriver.Safari()
-fb_names = []
-driver.get("http://www.google.com")
+def randomize_user_agent(fp):
+    fp.set_preference("general.useragent.override", random.choice(USER_AGENTS))
+    fp.update_preferences()
 
-with open('dists_mi.csv') as f:
-    next(f)
-    counter = 0
-    driver.get("http://www.google.com")
-    for line in f:
-        dist_name = line.split(',')[0]
+
+def extract_name(res):
+    if res == 'NOT FOUND':
+        return 'NOT FOUND'
+
+    if len(res.split('/')) >= 4:
+        fb_name = res.split('/')[3].split('?')[0]
+        return fb_name
+
+    elif len(res.split('/')) == 2:
+        print('short facebook URL.')
+        fb_name = res.split('/')[1].split('?')[0]
+        return fb_name
+
+    else:
+        print('Facebook URL is weird:', res)
+        return 'NOT FOUND'
+
+
+def sort_candidates(can):
+    for c in can:
+        user, likes = c
+        if 'district' in user or 'public' in user:
+            can.remove(c)
+            can.append((user, likes*10))
+
+    return sorted(can, key=lambda tup: tup[1], reverse=True)
+
+
+def find_best(res_list, e):
+    url_path = {'google': './/h3[@class="r"]/a[@href]',
+        'bing': './/h2/a[@href]',
+        'yahoo': '//div/span[@class=" fz-ms fw-m fc-12th wr-bw lh-17"]'}[e]
+    txt_path = {'google': './/div[@class="s"]/div/span[@class="st"]',
+        'bing': ['//div[@class="b_caption"]/p', '//div[@class="b_caption"]/div[@class="b_snippet"]/p'],
+        'yahoo': './/div[@class="compText aAbs"]/p[@class="lh-16"]'}[e]
+    result_likes = []
+    for l in res_list:
+        try:
+            if e is 'yahoo':
+                url = l.find_element(By.XPATH, url_path).text
+                text = l.find_element(By.XPATH, txt_path).text
+            if e is 'bing':
+                url = l.find_element(By.XPATH, url_path).text
+                text = l.find_element(By.XPATH, txt_path[0]).text
+                if not text:
+                    print('gotta add b snippet')
+                    text = l.find_element(By.XPATH, txt_path[1]).text
+            if e is 'google':
+                url = l.find_element(By.XPATH, url_path).get_attribute('href')
+                text = l.find_element(By.XPATH, txt_path).text
+        except StaleElementReferenceException:
+            break
+
+        print(url)
+        print(text)
+        # if 'schools' in url and 'facebook' in url:
+
+        if any(w in url for w in URL_STOP_WORDS) or ('facebook' not in url) or ('likes' not in text):
+            print('stopped')
+            continue
+        else:
+            num_likes = re.findall(r'(\d+)\slikes', l.text)
+            k_likes = re.findall(r'([\d\.]+)K\slikes', l.text)
+            if num_likes:
+                result_likes.append((url, int(num_likes[0])))
+            elif k_likes:
+                result_likes.append((url, int(float(k_likes[0]) * 1000)))
+
+    sor = sort_candidates(result_likes)
+    print('sorted candidates:\n', sor)
+    
+    if not sor:
+        print('No candidates.')
+        return 'NOT FOUND'
+
+    return sor[0][0]
+
+
+def get_fb_name(driver, query, e):
+    engine_url = {'google': 'http://www.google.com',
+        'bing': 'http://www.bing.com',
+        'yahoo': 'http://search.yahoo.com'}[e]
+    input_elem_name = {'google': 'q',
+        'bing': 'q',
+        'yahoo': 'p'}[e]
+    result_locator = {'google': '//div[@class="rc"]', 
+        'bing': '//li[@class="b_algo"]', 
+        'yahoo': '//div[@class="dd algo algo-sr fst Sr"]'}[e]
+
+    driver.get(engine_url)
+
+    input_element = driver.find_element_by_name(input_elem_name)
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, input_elem_name)))
+    input_element.send_keys(query)
+    try:
+        input_element.submit()
+    except StaleElementReferenceException:
+        pass
+    sleep(9)
+    try:
+        WebDriverWait(driver, 15).until(
+            EC.visibility_of_element_located((By.XPATH, result_locator)))
+        driver.execute_script("window.scrollTo(0, {});".format(random.random()*108))
+    except TimeoutException:
+        pass
+
+    rcs = driver.find_elements(By.XPATH, result_locator)
+    best_result = find_best(rcs, e)
+    
+    return extract_name(best_result)
+
+
+def main():
+    fp, driver = start_browser()
+    district_names = get_district_names('dists_remain.txt')
+    
+    fb_names = []
+    for i, dist_name in enumerate(district_names):
+        engine = random.choice(ENGINES)
         query = get_query(dist_name)
+        randomize_user_agent(fp)
 
         print('DISTRICT: ', dist_name)
-        fb_name = get_fb_name(query)
-        print('\nResult:', fb_name)
+        print('Engine: ', engine)
+
+        fb_name = get_fb_name(driver, query, engine)
         fb_names.append(fb_name)
+        with open('outfile2.txt', 'a') as f:
+            f.write(fb_name + '\n')
+
+        print('\nResult:', fb_name)
         print('-'*50, '\n')
 
-        counter += 1
-        sleep(10 + (random()*10 - 5))
+        # sleep(10 + (random.random()*10 - 5))
 
-        if not counter % 45:
-            sleep(15)
+    driver.close()
+    return None
 
-driver.close()
+main()
